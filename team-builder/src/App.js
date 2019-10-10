04a90881fe934e90bc74d1c93fd76b43af0ea6e3
cesarhj19 <cesarhj19@gmail.com>
2019-10-10 16:59:34 -0700

    MVP Completed

---
+++ team-builder/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form';
+import FormList from './components/FormList';
+
+function App() {
+  const [members, setMembers] = useState([{}, {}]);
+
+  return (
+    <div className="App">
+      <h1>Add Team Member</h1>
+
+      <Form members={members} setMembers={setMembers} />
+      <FormList members={members} />
+    </div>
+  );
+}
+
+export default App;
